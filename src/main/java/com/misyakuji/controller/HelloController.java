5700c91351d4f31b1bdab6822ba4e2e5d0321d81
Misyakuji <2810013347@qq.com>
2024-08-29 13:07:59 +0800

    update

--- src/main/java/com/misyakuji/controller/HelloController.java
+++ src/main/java/com/misyakuji/controller/HelloController.java
@@ -1,18 +1,18 @@
-package com.misyakuji.controller;
+// package com.misyakuji.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+// import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
 
 
-@RestController
-public class HelloController {
+// @RestController
+// public class HelloController {
 
-    @GetMapping
-    public String test(){
-        String currDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        return "现在时间:"+ currDate +" hello!\n";
-    }
-}
+//     @GetMapping
+//     public String test(){
+//         String currDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//         return "现在时间:"+ currDate +" hello!\n";
+//     }
+// }
